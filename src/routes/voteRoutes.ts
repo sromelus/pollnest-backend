735ad21c67a5fcfb6152f6d58fb21a0188b14635
Shardly Romelus <rshardlypicard89@gmail.com>
2024-10-30 11:15:52 -0400

    added rate limit for post votes end point

--- src/routes/voteRoutes.ts
+++ src/routes/voteRoutes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { getVotes, castVote } from '../controllers/voteController';
+import rateLimit from 'express-rate-limit';
 
 const router = Router();
 
+const voteLimiter = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000,
+    max: 3,
+});
+
 router.get('/', getVotes);
-router.post('/', castVote);
+router.post('/', voteLimiter, castVote);
 
 export default router;
