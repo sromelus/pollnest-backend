f4fdb967fb435feee550ecf4310114318f819a59
Shardly Romelus <rshardlypicard89@gmail.com>
2024-10-30 14:21:31 -0400

    added mongodb setup

--- app/src/index.ts
+++ app/src/index.ts
@@ -2,21 +2,36 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import voteRoutes from './routes/voteRoutes';
+import './loadEnvironment';
+import connectToDatabase from '../db/conn';
+import { envConfig, Environment } from '../config/environment';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const allowedOrigins = ['http://localhost:3000'];
+const ENV = process.env.NODE_ENV || 'development';
 
-app.use(cors({
-    origin: allowedOrigins,
-    credentials: true,
-}));
+const config = envConfig[ENV as Environment] || envConfig.development;
 
-app.use(cookieParser());
-app.use(express.json());
+const startServer = async () => {
+  try {
+    // const db = await connectToDatabase();
 
-app.use('/api/votes', voteRoutes);
+    app.use(cors({
+      origin: config.allowedOrigins,
+      credentials: true,
+    }));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    app.use(cookieParser());
+    app.use(express.json());
+    app.use('/api/votes', voteRoutes);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
