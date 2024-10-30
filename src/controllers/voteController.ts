7d5f24d8c00e4c2aaea789388c642ef1d2cf1a4f
Shardly Romelus <rshardlypicard89@gmail.com>
2024-10-30 01:04:43 -0400

    track user session to prevent double vote

--- src/controllers/voteController.ts
+++ src/controllers/voteController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 interface Votes {
   votedUser: object;
@@ -13,20 +14,41 @@ interface VoteTally {
 
 let votes: Votes[] = [];
 let voteTally: VoteTally = { kamala: 0, trump: 0 };
+let previousUserSession = new Set<string>();
 
 export const getVotes = (req: Request, res: Response) => {
-    res.json(voteTally);
+    let sessionId = req.cookies?.sessionId;
+    const hasVoted = previousUserSession.has(sessionId);
+    res.send({ voteTally, visitedUser: { disabledVote: hasVoted }});
 };
 
 export const castVote = (req: Request, res: Response) => {
+  let sessionId = req.cookies?.sessionId;
   const { votedUser, candidate, voterEthnicity, voterGender } = req.body as Votes;
 
-  if (req.body !== undefined && candidate !== '') {
-    votes.push({ votedUser, candidate, voterEthnicity, voterGender });
-    voteTally[candidate] += 1;
+  if (!sessionId) {
+    sessionId = uuidv4();
+  }
 
-    res.status(200).json({ success: true, voteTally });
+  if(previousUserSession.has(sessionId)) {
+    res.status(403).send({ success: false, message: 'User has already voted' });
+    return;
+  }
+
+  if (req.body !== undefined && candidate !== '') {
+      votes.push({ votedUser, candidate, voterEthnicity, voterGender });
+      voteTally[candidate] += 1;
+
+      res.cookie('sessionId', sessionId, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 60,
+        sameSite: 'none',
+      });
+
+      previousUserSession.add(sessionId);
+      res.status(200).send({ success: true, voteTally });
   } else {
-    res.status(400).json({ success: false, message: 'Invalid candidate' });
+    res.status(400).send({ success: false, message: 'Invalid candidate' });
   }
 };
